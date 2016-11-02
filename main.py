88df967d834139098e61c84c4cacb1cae791c350
lollokara <lorenzo@Lorenzos-MacBook-Pro.local>
2016-11-02 21:41:30 +0100

    desperate

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ def record():
     camera=VideoCamera()
     frame = camera.get_frame()
     render_template('rec.html')
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video = cv2.VideoWriter('video.avi',-1,5,(800,600))
+    fourcc = cv2.cv.CV_FOURCC(*'X264')
+    video = cv2.VideoWriter('video.avi',fourcc,5,(800,600))
     while True:
         render_template('rec.html')
         frame = camera.get_frame()
